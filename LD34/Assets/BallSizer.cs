e3849f5c349588c3b6e50c28b1eb237e60531b43
Nash429 <noah.the.buttkickingest@gmail.com>
2015-12-11 19:12:19 -0800

    first commit

---
+++ LD34/Assets/BallSizer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSizer : MonoBehaviour {
+
+    [SerializeField]
+    private float BallDiameter = 1.0f;
+
+    [SerializeField]
+    private float BallMass = 5.0f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = BallMass;
+        transform.localScale = new Vector3(BallDiameter, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
