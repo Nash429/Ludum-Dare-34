b2810a7e4c01299e4d0ed1e53b934d68cb9c6821
AdolfDupler <dupesofhazard@gmail.com>
2015-12-11 19:52:19 -0800

    Event System

---
+++ DuplerBranch/Local/Assets/Scripts/EventController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventController : MonoBehaviour {
+    public int score;
+    public float timer;
+    public GameObject player;
+    public bool hasWon = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasWon)
+        {
+            WinCommand();
+        }
+	
+	}
+    void WinCommand()
+    {
+        this.enabled = false;
+    }
+}
