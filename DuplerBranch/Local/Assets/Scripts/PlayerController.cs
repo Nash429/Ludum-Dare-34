9c7b980659dc2ddee57e49175300ec000549ce33
AdolfDupler <dupesofhazard@gmail.com>
2015-12-11 19:32:37 -0800

    First Scripts

---
+++ DuplerBranch/Local/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float scaleFactor = 1.5f;
+    private Rigidbody rb;
+    private Vector3 targetScale;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        targetScale = transform.localScale;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, .1f);
+
+    }
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+            
+            {
+            
+            
+            targetScale = transform.localScale * scaleFactor;
+        }
+        
+
+
+
+    }
+}
